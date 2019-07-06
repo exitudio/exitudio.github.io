7c0d115fb3b1c81e6de4fd556bc11cbfabe565a3
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2019-07-06 19:22:05 -0400

    update webpack to change root public folder

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   output: {
-    path: __dirname + '/docs',
+    path: __dirname + '/public',
     filename: '[name].bundle.js',
   },
   devServer: {
