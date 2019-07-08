31162e36af850c5fc51e2adb4772dc05a220a655
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2019-07-08 11:53:30 -0400

    check point

--- public/pages/fourier-series/index.js
+++ public/pages/fourier-series/index.js
@@ -28,9 +28,10 @@ async function startApp() {
     [100, -100],
     [100, -50]
   ];
-  // for (let i = 0; i < points.length; i++) {
-  //   points[i] = [points[i][0] + 100, points[i][1] + 200];
-  // }
+  for (let i = 0; i < points.length; i++) {
+    points[i] = [points[i][0] + 100, points[i][1] + 200];
+  }
+  points = moveToCenter(points);
   getPoint = getPointFactory(points);
   play(playAnimation);
 }
@@ -45,6 +46,22 @@ function trimPoints(points) {
   return newPoints;
 }
 
+function moveToCenter(points) {
+  const newPoints = [];
+  let avgX = 0;
+  let avgY = 0;
+  for (let i = 0; i < numSampling; i++) {
+    avgX += points[i][0];
+    avgY += points[i][1];
+  }
+  avgX = avgX / points.length;
+  avgY = avgY / points.length;
+  for (let i = 0; i < numSampling; i++) {
+    newPoints.push([points[i][0] - avgX, points[i][1] - avgY]);
+  }
+  return newPoints;
+}
+
 function playAnimation(t) {
   var x = 0;
   var y = 0;
@@ -52,48 +69,47 @@ function playAnimation(t) {
     var coefficient = getCoefficient(getPoint, n);
     var nextX;
     var nextY;
-    nextX = coefficient.x * Math.cos(n * 2 * Math.PI * t);
-    nextY = coefficient.y * Math.sin(n * 2 * Math.PI * t);
+    nextX = coefficient.an * Math.cos(n * 2 * Math.PI * t);
+    nextY = coefficient.bn * Math.sin(n * 2 * Math.PI * t);
     drawTerm(x, y, nextX, nextY);
     x = x + nextX;
     y = y + nextY;
   }
+  drawPoint({x,y}, "#0000ff")
   var point = getPoint(t);
   drawPoint(point);
+}
+
+function getCoefficient(f, n) {
+  var coefficient = { a0: 0, an: 0, bn: 0 };
+  for (var i = 0; i < numSampling; i++) {
+    var t = i / numSampling;
+    var points = f(t);
+    coefficient.an += 2 * points.x * Math.cos(n * 2 * Math.PI * t) * dt;
+    coefficient.bn += 2 * points.y * Math.sin(n * 2 * Math.PI * t) * dt;
+  }
+  return coefficient;
 }
 
 function drawTerm(x1, y1, x2, y2) {
   var radius = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
   context.beginPath();
-  context.arc(center.x + x1, center.y + y1, radius, 0, 2 * Math.PI);
-  context.stroke();
-
+  // context.arc(center.x + x1, center.y + y1, radius, 0, 2 * Math.PI);
   context.moveTo(center.x + x1, center.y + y1);
   context.lineTo(center.x + x1 + x2, center.y + y1 + y2);
   context.stroke();
+  context.closePath();
 }
 
-function getCoefficient(f, n) {
-  var coefficient = { x: 0, y: 0 };
-  for (var i = 0; i <= numSampling; i++) {
-    var t = i / numSampling;
-    var points = f(t);
-    coefficient.x += 2 * points.x * Math.cos(n * 2 * Math.PI * t) * dt;
-    coefficient.y += 2 * points.y * Math.sin(n * 2 * Math.PI * t) * dt;
-  }
-  return coefficient;
-}
-
-function drawPoint(point) {
+function drawPoint(point, color) {
   var x = point.x;
   var y = point.y;
   var pointSize = 3; // Change according to the size of the point.
-  context.fillStyle = "#ff2626"; // Red color
+  context.fillStyle = color?color:"#ff2626"; // Red color
 
   context.beginPath(); //Start path
   context.arc(center.x + x, center.y + y, pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
   context.fill(); // Close the path and fill.
-  console.log("--");
 }
 
 function play(callback) {
@@ -103,14 +119,12 @@ function play(callback) {
     callback(i / numSampling);
     i++;
     if (i >= numSampling) i = 0;
-  }, 200);
+  }, 1000);
 }
 
 function getPointFactory(points) {
   return function getPoint(t) {
-    if (t < 1) {
-      t = parseInt(t * points.length);
-    }
+    t = parseInt(t * points.length);
     return {
       x: points[t][0],
       y: points[t][1]
