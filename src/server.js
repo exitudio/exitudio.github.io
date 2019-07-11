eda9abaedc2769af63f88545c05940271f4b1852
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2019-07-11 11:21:31 -0400

    update server

--- src/server.js
+++ src/server.js
@@ -10,22 +10,14 @@ var app = express()
 
 
 
-app.use(express.static(path.join(__dirname, '../public')))
-app.use(fallback('../public/index.html', { root: __dirname }))
+app.use(express.static(path.join(__dirname, '../')))
+app.use(fallback('../index.html', { root: __dirname }))
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+  res.sendFile(path.join(__dirname, '../', 'index.html'))
 })
 
 var listener = app.listen(process.env.PORT || 3000, function(){
     console.log('Listening on port ' + listener.address().port) //Listening on port 8888
 })
-
-/**
- * Keep awake for heroku
- */
-var http = require("http")
-setInterval(function () {
-  http.get("http://ekkasit.herokuapp.com")
-}, 300000) // every 5 minutes (300000)
 
 module.exports = app
