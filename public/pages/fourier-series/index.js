b400c78dfb2e735141203225c87be7514b07bd7a
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2019-07-16 22:29:34 -0400

    seperate points

--- public/pages/fourier-series/index.js
+++ public/pages/fourier-series/index.js
@@ -1,9 +1,10 @@
+const MAX_DRAW = 100;
+const NUM_POINTS = 400;
 const center = { x: 400, y: 400 };
 const canvas = document.getElementById("fourier-series");
 const context = canvas.getContext("2d");
 context.fillStyle = "black";
 context.fillRect(0, 0, canvas.width, canvas.height);
-const MAX_DRAW = 100;
 
 startImageApp();
 document.getElementById("draw-btn").addEventListener("click", startDrawApp);
@@ -19,19 +20,20 @@ async function startDrawApp() {
   drawPoint({ x: 0, y: 0 });
   try {
     let points = await startGenPoints(canvas, context);
-    if (points.length > 400) {
-      points = trimPoints(points, 400);
-    }
     fourierStart(points);
   } catch (e) {}
 }
-function fourierStart(points) {
+function fourierStart(actualPoints) {
+  let points = actualPoints;
+  if (points.length > NUM_POINTS) {
+    points = trimPoints(actualPoints, NUM_POINTS);
+  }
   const fourier = dft(points);
   fourier.sort((a, b) => b.amp - a.amp);
-  play(playAnimation(fourier, points), points.length, 33);
+  play(playAnimation(fourier, points, actualPoints), points.length, 33);
 }
 
-function playAnimation(fourier, points) {
+function playAnimation(fourier, points, actualPoints) {
   const historyPoints = [];
   return function(t) {
     let x = 0;
@@ -55,7 +57,7 @@ function playAnimation(fourier, points) {
     }
     drawPoint({ x, y }, "#0000ff");
     drawPoint(points[parseInt(t * points.length)]);
-    drawPoints(points);
+    drawPoints(actualPoints);
 
     historyPoints.push({ x, y });
     if (historyPoints.length > MAX_DRAW) {
